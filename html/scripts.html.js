2320c13dcdae919671b303af3f83db2ccf5cb5c5
lasse <lasselvdbc@gmail.com>
2015-12-01 11:29:54 +0100

    edited make and added html-dir and files

---
+++ html/scripts.html.js
@@ -0,0 +1,6 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+<script>
+  	$(document).ready(function(){
+  		console.log('jquery initiated')
+   	})
+</script>
